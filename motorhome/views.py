b69fd7f39043160674dc6762b6c04824604d6a54
Forestriver <vitaly.romas1@gmail.com>
2020-03-15 22:59:49 +0200

    views update

--- motorhome/views.py
+++ motorhome/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(response):
-    return render(response, 'motorhome/layout.html')
+def index():
+    return render(request, 'layout.html', {})
+
+def about(request):
+    return render(request, 'motorhome/about.html', {})
+
+def admin(request):
+    return render(request, 'motorhome/admin.html', {})
+
+def contacts(request):
+    return render(request, 'motorhome/contacts.html', {})
+
+def homepage(request):
+    return render(request, 'motorhome/homepage.html', {})
