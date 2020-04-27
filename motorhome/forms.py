d49970f39d47964519f3dc53986dc1ca90ec1355
Forestriver <vitaly.romas1@gmail.com>
2020-04-28 02:14:49 +0300

    contact form updated

--- motorhome/forms.py
+++ motorhome/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    sender_name = forms.EmailField(required = False, label = 'Ваша e-mail адреса')
-    subject = forms.CharField(max_length=100)
+    sender = forms.CharField(label="Ім'я відправника",
+                             max_length=100,
+                             required = True,
+                             widget=forms.TextInput(attrs={'class':'form-control'}))         
+    email = forms.EmailField(label = "Ваша e-mail адреса",
+                             max_length=100,
+                             required=True,
+                             widget=forms.EmailInput(attrs={'class':'form-control'}))
     message = forms.CharField(widget=form.Textarea, label = 'Повідомлення')
