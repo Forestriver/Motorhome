fb817e6909f50eb3c928f642ae17f6c0a0492118
Forestriver <vitaly.romas1@gmail.com>
2020-05-12 01:06:53 +0300

    Procfile and settings update

--- motorhome_site/wsgi.py
+++ motorhome_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motorhome_root.motorhome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motorhome_site.settings')
 
 application = get_wsgi_application()
