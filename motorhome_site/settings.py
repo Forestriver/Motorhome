fd3870d31cf0c882521280cf6a4e3ac0002a30ba
Forestriver <vitaly.romas1@gmail.com>
2020-05-13 00:28:42 +0300

    settings update

--- motorhome_site/settings.py
+++ motorhome_site/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'motorhome/static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
