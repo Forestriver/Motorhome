c9b80ac9bc3238a01af7e6f964cdc2f7826c33b3
Forestriver <vitaly.romas1@gmail.com>
2020-05-13 01:39:35 +0300

    settings update

--- motorhome_site/settings.py
+++ motorhome_site/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "3no^975#wm-$@msrhboj$30=yymco$g)l3#6xszwi%a%070)r=")
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
