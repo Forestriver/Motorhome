cead757be20a6a4c159082eb85642f810aefc6d9
Forestriver <vitaly.romas1@gmail.com>
2020-05-12 10:11:23 +0300

    wsgi settings update

--- motorhome_site/wsgi.py
+++ motorhome_site/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motorhome_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motorhome_root.motorhome_site.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
