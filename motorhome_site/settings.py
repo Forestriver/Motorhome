96baf8d79648f5a67df3152acbf552bb5c443538
Forestriver <vitaly.romas1@gmail.com>
2020-05-14 01:36:42 +0300

    settings update

--- motorhome_site/settings.py
+++ motorhome_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "3no^975#wm-$@msrhboj$30=yymco$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
